d3af147df88284600fd40b3f4db10e98def1b850
infinity_project <iamsumitsam01@gmail.com>
2026-05-05 20:09:09 +0530

    fixed views.py conflict

--- core/apps.py
+++ core/apps.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
+
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-=======
-from django.apps import AppConfig
-
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
->>>>>>> 1b5ce9c (Initial commit)
     name = 'core'
