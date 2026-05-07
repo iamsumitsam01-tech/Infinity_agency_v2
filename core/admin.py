a576988722ac1645165a4b24a8893b29f5d1f988
infinity_project <iamsumitsam01@gmail.com>
2026-05-07 20:36:12 +0530

    Update Django admin users

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
 from .models import Service, Project, Contact
+
+
+# =========================
+# NORMAL MODELS
+# =========================
 
 admin.site.register(Service)
 admin.site.register(Project)
 
 
-from django.contrib import admin
-from .models import Contact
+# =========================
+# CONTACT ADMIN
+# =========================
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'status', 'created_at')
     list_filter = ('status',)
     search_fields = ('name', 'email')
+
+
+# =========================
+# USER ADMIN (GOOGLE LOGIN USERS)
+# =========================
+
+admin.site.unregister(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+    )
