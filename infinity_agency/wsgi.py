1e59220ee0b68b16f31fb3cc27657bd6aa0cd7ab
infinity_project <iamsumitsam01@gmail.com>
2026-06-19 15:01:20 +0530

    changes

--- infinity_agency/wsgi.py
+++ infinity_agency/wsgi.py
@@ -1,16 +1,10 @@
-ode"""
-WSGI config for infinity_agency project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infinity_agency.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'infinity_agency.settings'
+)
 
 application = get_wsgi_application()
