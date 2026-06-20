8d97c2af3d2b6ece54cca2de7503bd173c10943c
infinity_project <iamsumitsam01@gmail.com>
2026-06-20 15:04:23 +0530

    change

--- infinity_agency/settings.py
+++ infinity_agency/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = 'django-insecure-monlu2ak_kyt39=t@ykucm*w3w5lch1zuiyl**so*-7ustgbzd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    'bharatinfinity.com',
+    'www.bharatinfinity.com',
+    'infinity-agency-v2.onrender.com',
+    "*"
+]
 
 
 # Application definition
